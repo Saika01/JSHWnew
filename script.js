462f17131300cc197479812c256762f0a632d00d
Aoi <naztya2001@bk.ru>
2023-07-28 22:25:34 +0300

    Lesson 10

--- script.js
+++ script.js
@@ -1,88 +1,121 @@
 //1
 
-for (let i = 0; i < 11; i++) {
-    if (i===0)   
-    {
-        console.log(`${i} - это ноль`);
-    } else if (i%2!==0)
-    {
-        console.log(`${i} - это нечётное число`);
-    } else {
-        console.log(`${i} - это чётное число`);
-    }
+const numbers = {
+  keyin1: 1,
+  keyin2: 2,
+  keyin3: 3,
+  keyin4: 4,
+  keyin5: 5,
+  keyin6: 6,
+  keyin7: 7,
+};
+
+for (const key in numbers) {
+  if (numbers[key] >= 3) {
+    console.log(numbers[key]);
+  }
 }
 
 //2
 
-const array = [1, 2, 3, 4, 5, 6, 7];
-console.log(array.filter(num => (num<4)||(num>5)));
+const post = {
+  author: "John", // вывести этот текст
+  postId: 23,
+  comments: [
+    {
+      userId: 10,
+      userName: "Alex",
+      text: "lorem ipsum",
+      rating: {
+        likes: 10,
+        dislikes: 2, // вывести это число
+      },
+    },
+    {
+      userId: 5, // вывести это число
+      userName: "Jane",
+      text: "lorem ipsum 2", // вывести этот текст
+      rating: {
+        likes: 3,
+        dislikes: 1,
+      },
+    },
+  ],
+};
+
+console.log(post.author);
+console.log(post.comments[0].rating.dislikes);
+console.log(post.comments[1].userId);
+console.log(post.comments[1].text);
 
 //3
 
-const arr = [];
-for (let i = 0; i < 5; i++) {
-    arr[i] = Math.floor(Math.random()*10);
+const products = [
+  {
+    id: 3,
+    price: 200,
+  },
+  {
+    id: 4,
+    price: 900,
+  },
+  {
+    id: 1,
+    price: 1000,
+  },
+];
+
+products.forEach((element) => (element.price *= 0.85));
+
+console.log(products);
+
+//4
+
+const products2 = [
+  {
+    id: 3,
+    price: 127,
+    photos: ["1.jpg", "2.jpg"],
+  },
+  {
+    id: 5,
+    price: 499,
+    photos: [],
+  },
+  {
+    id: 10,
+    price: 26,
+    photos: ["3.jpg"],
+  },
+  {
+    id: 8,
+    price: 78,
+  },
+];
+
+const result = products2.filter(
+  (obj) => Object.keys(obj).includes("photos") && obj.photos.length !== 0
+);
+console.log(result);
+
+console.log(products2.sort((a, b) => a.price - b.price));
+
+//**5
+
+const en = ["mon", "tue", "wed", "thu", "fri", "sat", "sun"];
+const ru = [
+  "понедельник",
+  "вторник",
+  "среда",
+  "четверг",
+  "пятница",
+  "суббота",
+  "воскресенье",
+];
+const newObj = {};
+
+for (let i = 0; i < en.length; i++) {
+  newObj[en[i]] = ru[i];
 }
-console.log(arr);
 
-let sum = 0;
-for (let i = 0; i < arr.length; i++) {
-    sum+=arr[i];
-}
-console.log(sum);
-
-let min = arr[0];
-for (let i = 1; i < arr.length; i++) {
-    if(arr[i]<min){
-        min = arr[i];
-    }
-}
-console.log(min);
-
-let isThree = false;
-for (let i = 0; i < arr.length; i++) {
-    if (arr[i] === 3) {
-        isThree = true;
-        break;
-    }
-}
-if (isThree) console.log('There is a 3'); else console.log('There is no 3');
-
-//*
-
-for (let i = 0; i < 20; i++) {
-   console.log('x'.repeat(i));
-}
-
-//---
-
-const newArray = [];
-for (let i = 0; i < 10; i++) {
-    newArray[i] = Math.floor(Math.random()*10);
-}
-console.log(newArray);
-
-for (let i = 0; i < newArray.length; i++) {
-    if (newArray[i]%2 === 0){
-        console.log(newArray[i]*newArray[i]);
-    } else if (newArray[i]%3 === 0){
-        console.log(newArray[i]*newArray[i]*newArray[i]);
-    }
-}
-
-//---
-const anotherArr = [1, 2, 3, 2, 4, 3, 5, 6, 3, 2, 3];
-console.log(anotherArr);
-let countThree = 0;
-for (let i = 0; i < anotherArr.length; i++) {
-    if (anotherArr[i] ===3) {
-        countThree++;
-    }
-}
-console.log(countThree);
-
-//---
-const theNewestArray = [1, 2, 3, 4, 5];
-const removedElements = theNewestArray.map(num => num);
-removedElements.splice(1,2);
-console.log(removedElements);
+console.log(newObj);
